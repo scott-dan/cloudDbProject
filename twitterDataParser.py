42df9dc436c30730918dd2ad5533b1ddbb6f0d4a
scott-dan <58237822+scott-dan@users.noreply.github.com>
2021-11-14 20:10:49 -0800

    Change formatting
    All json formatting is done using json.loads() on raw json data and json.dumps() on the dictionary object that describes the tweet data format

--- twitterDataParser.py
+++ twitterDataParser.py
@@ -6,45 +6,45 @@ inFile = 'Eurovision3'
 outFile = inFile + '_clean' + '.json'
 
 def main():
-	tweets = []
-	print("Scanning {} for tweets...\n".format(inFile))
-	with open(inFile + '.json') as f_in:
-		for line in f_in:
-			if "created_at" not in line:
-				continue
-			else:
-				tweet = json.loads(line)
-				clean_tweet = [
-				'{' + "created_at" + ':' + tweet["created_at"],
-				"id" + ':' + tweet["id_str"],
-				"text" + ':' + tweet["text"],
-					"user" + ':' + '{' +
-					"id" + ':' + tweet["user"]["id_str"], #concat or comma?
-					"name" + ':' + tweet["user"]["name"],
-					"screen_name" + ':' + tweet["user"]["screen_name"],
-					#"location" + ':' + tweet["user"]["location"] + '}', #Need to check for null and still insert in user section
-				#"hashtags" + ':' + tweet["entities"]["hashtags"], #I need to figure out how to represent this list
-				"timestamp_ms" + ':' + tweet["timestamp_ms"]
-				]
-				if(tweet["in_reply_to_status_id"]):
-					clean_tweet.append("in_reply_to_status_id" + ':' + tweet["in_reply_to_status_id_str"])
-				if(tweet["in_reply_to_user_id"]):
-					clean_tweet.append("in_reply_to_user_id" + ':' + tweet["in_reply_to_user_id_str"])
-				if(tweet["in_reply_to_screen_name"]):
-					clean_tweet.append("in_reply_to_screen_name" + ':' + tweet["in_reply_to_screen_name"])
-				clean_tweet.append('}')
-			tweets.append(clean_tweet)
-		print(tweets[42069])
-	f_in.close()
+    tweets = []
+    print("Scanning {} for tweets...\n".format(inFile))
+    with open(inFile + '.json') as f_in:
+        for line in f_in:
+            if "created_at" not in line:
+                continue
+            else:
+                tweet = json.loads(line)
+                tweetObj = {
+                    "created_at": tweet["created_at"],
+                    "id": tweet["id"],
+                    "text": tweet["text"],
+                    "user": {
+                        "id": tweet["user"]["id"],
+                        "name": tweet["user"]["name"],
+                        "screen_name": tweet["user"]["screen_name"],
+                        "location": tweet["user"]["location"]
+                    },
+                    "hashtags": tweet["entities"]["hashtags"],
+                    "timestamp_ms": tweet["timestamp_ms"],
+                    "in_reply_to_status_id": tweet["in_reply_to_status_id"],
+                    "in_reply_to_user_id": tweet["in_reply_to_user_id"],
+                    "in_reply_to_screen_name": tweet["in_reply_to_screen_name"]
+                }
+                #print(tweetObj)
+                clean_tweet = json.dumps(tweetObj)
+                #print(clean_tweet)
+                tweets.append(clean_tweet)
+        #print(tweets[42069])
+    f_in.close()
     
     
 	#Will go back to writing out once data is properly formatted
-    #with open(outFile, "w", encoding = 'utf-8') as f_out:
-    #    f_out.write('[')
-    #    for tweet in tweets:
-    #        json.dump(tweet, ',')
-    #    f_out.write(']')
-    #f_out.close()
+    with open(outFile, "w", encoding = 'utf-8') as f_out:
+        f_out.write('[')
+        for tweet in tweets:
+            f_out.write(tweet + ',\n')
+        f_out.write(']')
+    f_out.close()
     
     
 if __name__ == "__main__":
@@ -54,4 +54,3 @@ if __name__ == "__main__":
     start_time = time.time()
     main()
     print("Completed scan in %s seconds" % (time.time() - start_time))
-    
